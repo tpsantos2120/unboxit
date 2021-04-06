8a2ff6ee3add3fe58cdcb0eb27746d04fad3877c
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-06 21:33:55 +0100

    Added function to verify connectivity to database

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
+import os
+from flask_pymongo import PyMongo
 from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    test_db_connection()
+    return "test_db_connection() worked!"
+
+
+def test_db_connection():
+    existing_user = mongo.db.users.find_one(
+            {"username": "thiago"})
+    print(existing_user)
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=os.environ.get("DEBUG"))
+
