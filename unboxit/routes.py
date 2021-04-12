467b2d6af135b39d5b34c339e7ce32571d65e2a6
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-12 19:39:19 +0100

    Updated code identention

--- unboxit/routes.py
+++ unboxit/routes.py
@@ -1,7 +1,8 @@
-from flask import session
+from flask import session, request
 from unboxit import app, mongo
 from unboxit.json_encoder import JSONEncoder
 from werkzeug.security import generate_password_hash
+
 
 users = mongo.db.users
 
@@ -9,18 +10,17 @@ users = mongo.db.users
 @app.route("/api/users", methods=['GET', 'POST'])
 def register_user():
     if request.method == "POST":
-    user_exist = users.find_one({'username': 'noxx'})
-
-    if user_exist:
-        return "User Exist"
-    else:
-        hashed_password = generate_password_hash("test")
-        new_user = {
-            'first_name': "Thi",
-            'last_name': "ago",
-            'username': "noxx",
-            'password': hashed_password
-        }
-        users.insert_one(new_user)
-        session["username"] = "Thiago"
-        return "Registered"
+        user_exist = users.find_one({'username': 'noxx'})
+        if user_exist:
+            return "User Exist"
+        else:
+            hashed_password = generate_password_hash("test")
+            new_user = {
+                'first_name': "Thi",
+                'last_name': "ago",
+                'username': "noxx",
+                'password': hashed_password
+            }
+            users.insert_one(new_user)
+            session["username"] = "Thiago"
+            return "Registered"
