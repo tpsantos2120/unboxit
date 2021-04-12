bad4fbcf59dcdc00fc9e0f8a2b1d2237b0f2c1f3
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-12 18:09:10 +0100

    Removed app configs and routes

--- app.py
+++ app.py
@@ -1,40 +1,5 @@
-import os, json
-from bson import ObjectId
-from flask_pymongo import PyMongo
-from flask import Flask
-if os.path.exists("env.py"):
-    import env
-
-
-app = Flask(__name__)
-
-
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-
-mongo = PyMongo(app)
-
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
-
-
-@app.route("/")
-def home():
-    return test_db_connection()
-
-
-def test_db_connection():
-    existing_user = mongo.db.users.find_one(
-            {"username": "thiago"})
-    user = JSONEncoder().encode(existing_user)
-    return user
-
-
+import os
+from unboxit import app
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
