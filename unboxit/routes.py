f9c307c2fc567222334d66ef3b43b308f729ed9e
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-12 21:07:42 +0100

    Added flash messages and JWT for tokens

--- unboxit/routes.py
+++ unboxit/routes.py
@@ -1,16 +1,25 @@
-from flask import session, request
+from flask import session, request, flash
 from unboxit import app, mongo
+import jwt
 from unboxit.json_encoder import JSONEncoder
 from werkzeug.security import generate_password_hash
 
 
 users = mongo.db.users
+JWT_ALGORITHM = "HS256"
 
 
 @app.route("/api/users", methods=['GET', 'POST'])
 def register_user():
-    if request.method == "POST":
+
+    if 'username' in session:
+        flash('You are already registered!')
+        return "You are already registered!"
+
+    if request.method  == "GET":
         user_exist = users.find_one({'username': 'noxx'})
+
+    if request.method == "POST":
         if user_exist:
             return "User Exist"
         else:
@@ -23,4 +32,6 @@ def register_user():
             }
             users.insert_one(new_user)
             session["username"] = "Thiago"
-            return "Registered"
+            flash('You have been successfully registered.')
+            return "You have been successfully registered."
+    return "Nothing"
