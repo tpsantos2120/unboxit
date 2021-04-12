5929a1a5ec9886b88a11457b5a27d5375d517cc3
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-12 17:59:01 +0100

    Added app configurations and environment variables

---
+++ unboxit/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask_pymongo import PyMongo
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
+mongo = PyMongo(app)
