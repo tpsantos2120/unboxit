56157db1655b5decb27cb05bc3438514c46802ee
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-13 15:48:26 +0100

    Updated app.config and app.run to set depending on my ENV_STATE variable in the env.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import os
 from unboxit import app
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=os.environ.get("DEBUG"))
+    if os.environ.get('ENV_STATE') == "production":
+        app.config["ENV"] = "production"
+        app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')))
+    elif os.environ.get('ENV_STATE') == "development":
+        app.config["ENV"] = "development"
+        app.run(debug=os.environ.get("DEBUG"))
 
