de90b24a63c515f979ffc727d3cb959877cb5016
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 23:30:58 +0100

    Modified Movies request to check custom error message

--- unboxit/resources/movie.py
+++ unboxit/resources/movie.py
@@ -4,21 +4,31 @@ from flask.json import jsonify
 from unboxit.models.models import Movie, User
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
-
+from mongoengine.errors import DoesNotExist
+from unboxit.resources.errors import InternalServerError, MovieNotExistsError
 
 class MoviesApi(Resource):
     def get(self):
         movies = Movie.objects().to_json()
         return Response(movies, mimetype="application/json", status=200)
-    
+
     @jwt_required()
     def post(self):
-        user_id = get_jwt_identity()
-        body = request.get_json()
-        user = User.objects.get(id=user_id)
-        movie =  Movie(**body)
-        movie.save()
-        user.update(push__movies=movie)
-        user.save()
-        id = movie.id
-        return {'id': str(id)}, 200
+        try:
+            print()
+            user_id = get_jwt_identity()
+            body = request.get_json()
+            user = User.objects.get(id=user_id)
+            check_movie = Movie.objects.get(imdb_id=body['imdb_id'])
+            # if not check_movie:
+            #     print(check_movie)
+            #movie =  Movie(**body)
+            # movie.save()
+            # user.update(push__movies=movie)
+            # user.save()
+            #id = movie.id
+            return {'id': "str(id)"}, 200
+        except DoesNotExist:
+            raise MovieNotExistsError
+        except Exception as e:
+            raise InternalServerError
