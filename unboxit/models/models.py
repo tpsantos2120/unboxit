3cfacfb433de59747c815f742ea750a89ffc617a
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 17:33:45 +0100

    Added Movie and TvSeries models

--- unboxit/models/models.py
+++ unboxit/models/models.py
@@ -1,7 +1,50 @@
 from .db import db
 
+
 class User(db.Document):
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
     username = db.StringField(required=True, unique=True)
     password = db.StringField(required=True)
+    movies = db.ListField(db.ReferenceField('Movies'), required=True)
+    tv_series = db.ListField(db.ReferenceField('TvSeries'), required=True)
+
+
+class Movie(db.Document):
+    movie_type = db.StringField(required=True)
+    title = db.StringField(required=True)
+    description = db.StringField(required=True)
+    year = db.StringField(required=True)
+    release_date = db.StringField(required=True)
+    imdb_id = db.StringField(required=True, unique=True)
+    imbd_rating = db.StringField(required=True)
+    vote_count = db.StringField(required=True)
+    popularity = db.StringField(required=True)
+    youtube_trailer_key = db.StringField(required=True)
+    runtime = db.IntField(required=True)
+    rated = db.ListField(db.StringField(), required=True)
+    genres = db.ListField(db.StringField(), required=True)
+    stars = db.ListField(db.StringField(), required=True)
+    directors = db.ListField(db.StringField(), required=True)
+    countries = db.ListField(db.StringField(), required=True)
+    language = db.ListField(db.StringField(), required=True)
+
+
+class TvSeries(db.Document):
+    movie_type = db.StringField(required=True)
+    title = db.StringField(required=True)
+    description = db.StringField(required=True)
+    year = db.StringField(required=True)
+    release_date = db.StringField(required=True)
+    imdb_id = db.StringField(required=True, unique=True)
+    imbd_rating = db.StringField(required=True)
+    vote_count = db.StringField(required=True)
+    popularity = db.StringField(required=True)
+    youtube_trailer_key = db.StringField(required=True)
+    runtime = db.IntField(required=True)
+    stars = db.ListField(db.StringField(), required=True)
+    creators = db.ListField(db.StringField(), required=True)
+    countries = db.ListField(db.StringField(), required=True)
+    language = db.ListField(db.StringField(), required=True)
+    production_companies = db.ListField(db.StringField(), required=True)
+    networks = db.ListField(db.StringField(), required=True)
