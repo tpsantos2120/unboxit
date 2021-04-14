3240ae8aba880e5dbf09bde503502af95a9cae8d
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 20:26:55 +0100

    Added Login API Resource to check password and provide response token if user exist

--- unboxit/resources/user.py
+++ unboxit/resources/user.py
@@ -1,4 +1,6 @@
+import datetime
 from flask import Response, request
+from flask_jwt_extended import create_access_token
 from unboxit.models.models import User
 from flask_restful import Resource
 
@@ -10,3 +12,15 @@ class RegisterUserApi(Resource):
         user.save()
         id = user.id
         return {'id': str(id)}, 200
+
+
+class LoginUserApi(Resource):
+    def post(self):
+            body = request.get_json()
+            user = User.objects.get(email=body.get('email'))
+            authorized = user.check_password(body.get('password'))
+            if not authorized:
+                return {'Access': "Denied"}, 403
+            expires = datetime.timedelta(days=7)
+            access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+            return {'token': access_token}, 200
