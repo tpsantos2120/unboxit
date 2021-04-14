f3198150e89fddd4d49a25109f6611c4193e41cb
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 20:35:22 +0100

    Imported and Initialised Bcrypt for encryption

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from unboxit.models.db import initialize_db
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+
 
 
 
@@ -16,6 +18,7 @@ from unboxit.resources.routes import initialize_routes
 
 
 api = Api(app)
+bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 
