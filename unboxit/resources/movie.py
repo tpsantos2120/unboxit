412d801075f80b5d0c5e00ebb7060b6fe4a7a18d
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 21:56:17 +0100

    Updated movies post request to make sure user has token in the header for authorization

--- unboxit/resources/movie.py
+++ unboxit/resources/movie.py
@@ -1,5 +1,6 @@
 
 from flask import Response, request
+from flask.json import jsonify
 from unboxit.models.models import Movie, User
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
@@ -10,8 +11,14 @@ class MoviesApi(Resource):
         movies = Movie.objects().to_json()
         return Response(movies, mimetype="application/json", status=200)
     
+    @jwt_required()
     def post(self):
+        user_id = get_jwt_identity()
         body = request.get_json()
-        movie = Movie(**body).save()
+        user = User.objects.get(id=user_id)
+        movie =  Movie(**body)
+        movie.save()
+        user.update(push__movies=movie)
+        user.save()
         id = movie.id
         return {'id': str(id)}, 200
