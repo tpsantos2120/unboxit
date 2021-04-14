5023c51a281cd494131ddfc0d80e3df86336dffd
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 19:15:39 +0100

    Updated resource routes to register user

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -1,5 +1,6 @@
 from .movie import MoviesApi
-
+from .user import RegisterUserApi
 
 def initialize_routes(api):
     api.add_resource(MoviesApi, '/api/movies')
+    api.add_resource(RegisterUserApi, '/api/auth/register')
