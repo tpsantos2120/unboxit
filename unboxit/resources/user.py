f7bcdd950046e33edd6949b0094bd3df58253540
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 19:26:10 +0100

    Called hash function for user password

--- unboxit/resources/user.py
+++ unboxit/resources/user.py
@@ -6,6 +6,7 @@ class RegisterUserApi(Resource):
     def post(self):
         body = request.get_json()
         user =  User(**body)
+        user.hash_password()
         user.save()
         id = user.id
         return {'id': str(id)}, 200
