231e4eb133b9f21faef0fcf56018352f26a1a92a
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 19:01:32 +0100

    Added post API to add movies

--- unboxit/resources/movie.py
+++ unboxit/resources/movie.py
@@ -1,14 +1,17 @@
 
-from flask import Response
-from unboxit.models.models import Movie
+from flask import Response, request
+from unboxit.models.models import Movie, User
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
 
 
 class MoviesApi(Resource):
     def get(self):
-        query = Movie.objects()
         movies = Movie.objects().to_json()
-        print(movies)
-        print(query)
         return Response(movies, mimetype="application/json", status=200)
+    
+    def post(self):
+        body = request.get_json()
+        movie = Movie(**body).save()
+        id = movie.id
+        return {'id': str(id)}, 200
