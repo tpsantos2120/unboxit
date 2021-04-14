b9013b3b3b926c5eebe4163d78773c29ed3a5485
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-14 15:30:52 +0100

    Added all run.py to separate app from configs.

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+from app import app
+
+if __name__ == '__main__':
+    if os.environ.get('ENV_STATE') == "production":
+        app.config["ENV"] = "production"
+        app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')))
+    elif os.environ.get('ENV_STATE') == "development":
+        app.config["ENV"] = "development"
+        app.run(debug=os.environ.get("DEBUG"))
