1b9e056bf4a85ffd8f20173e39733c6c68e7be33
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-15 20:18:54 +0100

    Added error handling when user is not authorised or does not exist

--- unboxit/resources/user.py
+++ unboxit/resources/user.py
@@ -3,6 +3,8 @@ from flask import Response, request
 from flask_jwt_extended import create_access_token
 from unboxit.models.models import User
 from flask_restful import Resource
+from mongoengine.errors import DoesNotExist
+from unboxit.resources.errors import InternalServerError,UnauthorizedError
 
 
 class RegisterUserApi(Resource):
@@ -17,15 +19,20 @@ class RegisterUserApi(Resource):
 
 class LoginUserApi(Resource):
     def post(self):
-        body = request.form
-        user = User.objects.get(email=body.get('email'))
-        authorized = user.check_password(body.get('password'))
-        if not authorized:
-            return {'response': "Access Denied"}, 403
-        expires = datetime.timedelta(days=7)
-        access_token = create_access_token(
-            identity=str(user.id), expires_delta=expires)
-        return {
-            "response": "Logged in successfully",
-            'token': access_token
-        }, 200
+        try:
+            body = request.form
+            user = User.objects.get(email=body.get('email'))
+            authorized = user.check_password(body.get('password'))
+            if not authorized:
+                return {'response': "Access Denied"}, 403
+            expires = datetime.timedelta(days=7)
+            access_token = create_access_token(
+                identity=str(user.id), expires_delta=expires)
+            return {
+                "response": "Logged in successfully",
+                'token': access_token
+            }, 200
+        except (UnauthorizedError, DoesNotExist):
+            raise UnauthorizedError
+        except Exception as e:
+            raise InternalServerError
