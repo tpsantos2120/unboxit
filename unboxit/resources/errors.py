d9a91fdad5b7c097aae26997b4f99203ee0cfa21
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-15 05:41:50 +0100

    Updated error message for movie that already exists

--- unboxit/resources/errors.py
+++ unboxit/resources/errors.py
@@ -1,19 +1,25 @@
 from werkzeug.exceptions import HTTPException
 
-
 class InternalServerError(HTTPException):
     pass
-
 class MovieNotExistsError(HTTPException):
+    pass
+class MovieAlreadyExistsError(HTTPException):
+    pass
+class MovieAlreadyExistsError(HTTPException):
     pass
 
 errors = {
     "InternalServerError": {
-        "message": "Something went wrong",
+        "message": "Something went wrong internally",
         "status": 500
     },
      'MovieNotExistsError': {
-         'message': "Movie with given id doesn't exists",
+         'message': "Movie with given id doesn't exist",
          'status': 400
+     },
+     "MovieAlreadyExistsError": {
+         "message": "Movie with given id already exists",
+         "status": 400
      }
 }
