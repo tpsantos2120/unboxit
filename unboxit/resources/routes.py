2b4d2ea113c3ac5a3768a62f0051979cc7140310
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-15 05:39:23 +0100

    Reverted back to previous routes

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -1,10 +1,10 @@
-from .watchlist import WatchListApi
+from .movie import MoviesApi
 from .user import RegisterUserApi
 from .user import LoginUserApi
 
 
 def initialize_routes(api):
-    api.add_resource(WatchListApi, '/api/watchlist')
+    api.add_resource(MoviesApi, '/api/movies')
     api.add_resource(RegisterUserApi, '/api/auth/register')
     api.add_resource(LoginUserApi, '/api/auth/login')
 
