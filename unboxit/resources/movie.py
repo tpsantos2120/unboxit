8d944a14ac81c058522f7e68e61669e6a06f6821
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-15 05:39:03 +0100

    Reverted back to the previous MoviesApi

---
+++ unboxit/resources/movie.py
@@ -0,0 +1,32 @@
+
+from flask import Response, request
+from flask.json import jsonify
+from unboxit.models.models import Movie, User
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import Resource
+from mongoengine.errors import DoesNotExist, NotUniqueError
+from unboxit.resources.errors import InternalServerError, MovieNotExistsError, MovieAlreadyExistsError
+
+class MoviesApi(Resource):
+    def get(self):
+        movies = Movie.objects().to_json()
+        return Response(movies, mimetype="application/json", status=200)
+
+    @jwt_required()
+    def post(self):
+        #try:
+            user_id = get_jwt_identity()
+            body = request.get_json()
+            user = User.objects.get(id=user_id)
+            movie = Movie(**body, added_by=user)
+            movie.save()
+            user.update(add_to_set__movies=movie)
+            user.save()
+            id = movie.id
+            return {'id': str(id)}, 200
+        # except DoesNotExist:
+        #     raise MovieNotExistsError
+        # except NotUniqueError:
+        #     raise MovieAlreadyExistsError
+        # except Exception as e:
+        #     raise InternalServerError
