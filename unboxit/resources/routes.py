0715595f4b6be96df6b5f79d8c43d443f85863fb
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-15 14:37:39 +0100

    Added home root route to the resource registry

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -1,10 +1,12 @@
 from .movie import MoviesApi
 from .user import RegisterUserApi
 from .user import LoginUserApi
+from .home import Home
 
 
 def initialize_routes(api):
+    print(api)
     api.add_resource(MoviesApi, '/api/movies')
     api.add_resource(RegisterUserApi, '/api/auth/register')
     api.add_resource(LoginUserApi, '/api/auth/login')
-
+    api.add_resource(Home, '/')
