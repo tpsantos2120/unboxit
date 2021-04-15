22c836becf94b064e22fa971e9e81bf6be19bff4
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-16 00:18:08 +0100

    Added routes for dashboard and login

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -2,6 +2,9 @@ from .movie import MoviesApi
 from .user import RegisterUserApi
 from .user import LoginUserApi
 from .home import Home
+from .login import Login
+from .dashboard import Dashboard
+
 
 
 def initialize_routes(api):
@@ -10,3 +13,5 @@ def initialize_routes(api):
     api.add_resource(RegisterUserApi, '/api/auth/register')
     api.add_resource(LoginUserApi, '/api/auth/login')
     api.add_resource(Home, '/')
+    api.add_resource(Login, '/login')
+    api.add_resource(Dashboard, '/dashboard')
