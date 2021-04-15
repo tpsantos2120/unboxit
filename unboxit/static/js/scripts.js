3919d9378597f08408045419a9fdbe16c5283119
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-15 20:04:44 +0100

    Removed the particles funtion

--- unboxit/static/js/scripts.js
+++ unboxit/static/js/scripts.js
@@ -1,33 +1,4 @@
-window.onload = function () {
-  Particles.init({
-    // normal options
-    selector: ".background",
-    maxParticles: 450,
-    // options for breakpoints
-    responsive: [
-      {
-        breakpoint: 768,
-        options: {
-          maxParticles: 200,
-          color: "#48F2E3",
-          connectParticles: false,
-        },
-      },
-      {
-        breakpoint: 425,
-        options: {
-          maxParticles: 100,
-          connectParticles: true,
-        },
-      },
-      {
-        breakpoint: 320,
-        options: {
-          maxParticles: 0,
 
-          // disables particles.js
-        },
-      },
-    ],
-  });
-};
+function form_submit() {
+  document.getElementById("login_form").submit();
+}
