7fda367b13d8795b89bb9f4dbb66cfe1c3809227
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-16 23:15:20 +0100

    Added resources for querying movies from imdb

--- unboxit/resources/imdb.py
+++ unboxit/resources/imdb.py
@@ -2,6 +2,54 @@ from flask import Response, request, render_template, make_response, url_for, re
 from flask_restful import Resource
 import requests, os
 
+class GetMoviesByTitle(Resource):
+    def get(self):
+        configs = GetIMDBConfigs()
+        url = configs.get_url()
+        print(url)
+        headers = configs.get_headers()
+        print(headers)
+        querystring = {"type": "get-movies-by-title", "title": "matrix"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        return response.json()
+
+class GetMovieDetails(Resource):
+    def get(self):
+        configs = GetIMDBConfigs()
+        url = configs.get_url()
+        print(url)
+        headers = configs.get_headers()
+        print(headers)
+        querystring = {"type": "get-movie-details", "imdb": "tt2935510"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        return response.json()
+
+class GetMovieImages(Resource):
+    def get(self):
+        configs = GetIMDBConfigs()
+        url = configs.get_url()
+        print(url)
+        headers = configs.get_headers()
+        print(headers)
+        querystring = {"type": "get-movies-images-by-imdb", "imdb": "tt2935510"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        return response.json()
+
+class GetSimilarMovies(Resource):
+    def get(self):
+        configs = GetIMDBConfigs()
+        url = configs.get_url()
+        print(url)
+        headers = configs.get_headers()
+        print(headers)
+        querystring = {"type": "get-similar-movies", "imdb": "tt2935510","page":"1"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        return response.json()
+
 
 class GetIMDBConfigs():
     def get_headers(self):
