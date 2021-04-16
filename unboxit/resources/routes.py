04e40beb5b35241ed25458684342035504b6a5f6
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-16 23:16:47 +0100

    Added routes for all movies and tv shows resources.

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -5,7 +5,8 @@ from .home import Home
 from .login import Login
 from .register import Register
 from .dashboard import Dashboard
-
+from .imdb import GetMoviesByTitle, GetShowsByTitle, GetShowDetails, GetMovieDetails \
+, GetMovieImages, GetShowImages, GetSimilarMovies, GetSimilarShows
 
 
 def initialize_routes(api):
@@ -17,3 +18,11 @@ def initialize_routes(api):
     api.add_resource(Login, '/login')
     api.add_resource(Register, '/register')
     api.add_resource(Dashboard, '/dashboard')
+    api.add_resource(GetMoviesByTitle, '/get-movies-by-title')
+    api.add_resource(GetMovieDetails, '/get-movie-details')
+    api.add_resource(GetMovieImages, '/get-movie-images')
+    api.add_resource(GetSimilarMovies, '/get-similar-movies')
+    api.add_resource(GetShowsByTitle, '/get-shows-by-title')
+    api.add_resource(GetShowDetails, '/get-show-details')
+    api.add_resource(GetShowImages, '/get-show-images')
+    api.add_resource(GetSimilarShows, '/get-similar-shows')
