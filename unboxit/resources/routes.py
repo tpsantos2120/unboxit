5ee5194f94f5824ed494e2fbe9987957659ced0a
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-17 04:02:21 +0100

    Updated routes for images to match endpoints

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -5,8 +5,8 @@ from .home import Home
 from .login import Login
 from .register import Register
 from .dashboard import Dashboard
-from .imdb import GetMoviesByTitle, GetShowsByTitle, GetShowDetails, GetMovieDetails \
-, GetMovieImages, GetShowImages, GetSimilarMovies, GetSimilarShows
+from .imdb import GetMoviesByTitle, GetMoviesImagesByImdb, GetShowsByTitle, GetShowDetails, GetMovieDetails \
+, GetMoviesImagesByImdb, GetShowImagesByImdb, GetSimilarMovies, GetSimilarShows
 
 
 def initialize_routes(api):
@@ -18,11 +18,11 @@ def initialize_routes(api):
     api.add_resource(Login, '/login')
     api.add_resource(Register, '/register')
     api.add_resource(Dashboard, '/dashboard')
-    api.add_resource(GetMoviesByTitle, '/get-movies-by-title/<title>')
-    api.add_resource(GetMovieDetails, '/get-movie-details/<id>')
-    api.add_resource(GetMovieImages, '/get-movie-images/<id>')
-    api.add_resource(GetSimilarMovies, '/get-similar-movies/<id>')
-    api.add_resource(GetShowsByTitle, '/get-shows-by-title/<title>')
-    api.add_resource(GetShowDetails, '/get-show-details/<id>')
-    api.add_resource(GetShowImages, '/get-show-images/<id>')
-    api.add_resource(GetSimilarShows, '/get-similar-shows/<id>')
+    api.add_resource(GetMoviesByTitle, '/get-movies-by-title/<title>', endpoint="get-movies-by-title")
+    api.add_resource(GetMovieDetails, '/get-movie-details/<id>', endpoint="get-movie-details")
+    api.add_resource(GetMoviesImagesByImdb, '/get-movies-images-by-imdb/<id>', endpoint="get-movies-images-by-imdb")
+    api.add_resource(GetSimilarMovies, '/get-similar-movies/<id>', endpoint="get-similar-movies")
+    api.add_resource(GetShowsByTitle, '/get-shows-by-title/<title>', endpoint="get-shows-by-title")
+    api.add_resource(GetShowDetails, '/get-show-details/<id>', endpoint="get-show-details")
+    api.add_resource(GetShowImagesByImdb, '/get-show-images-by-imdb/<id>', endpoint="get-show-images-by-imdb")
+    api.add_resource(GetSimilarShows, '/get-similar-shows/<id>', endpoint="get-similar-shows")
