d4aa4c4d443fa55e257538620b489649b8bc4155
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-17 04:01:44 +0100

    Added a function to format query strings and added functionality to search for movie title and return back with images and ids.

--- unboxit/resources/imdb.py
+++ unboxit/resources/imdb.py
@@ -1,79 +1,137 @@
 from flask import Response, request, render_template, make_response, url_for, redirect
 from flask_restful import Resource
-import requests, os
+from ratelimit import limits, sleep_and_retry
+import requests
+import os
+
 
 class GetMoviesByTitle(Resource):
+    @sleep_and_retry
+    @limits(calls=5, period=1)
     def get(self, title):
         print(title)
+        response_result = []
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-movies-by-title", "title": "matrix"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        return response.json()
+        query_string = configs.get_query_string(
+            request.endpoint, None, title, None)
+        query_by_title_response = requests.request(
+            "GET", url, headers=headers, params=query_string)
+        movies = query_by_title_response.json()
+        for movie in movies['movie_results']:
+            response = requests.request(
+                "GET", request.url_root + "/get-movies-images-by-imdb/"+movie['imdb_id'])
+            movies_details = response.json()
+            response_result.append(movies_details)
+        return response_result
+
 
 class GetMovieDetails(Resource):
+    @sleep_and_retry
+    @limits(calls=5, period=1)
     def get(self, id):
+        print(id)
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-movie-details", "imdb": "tt2935510"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        query_string = configs.get_query_string(
+            request.endpoint, id, None, None)
+        response = requests.request(
+            "GET", url, headers=headers, params=query_string)
         return response.json()
 
-class GetMovieImages(Resource):
+
+class GetMoviesImagesByImdb(Resource):
+    @sleep_and_retry
+    @limits(calls=5, period=1)
     def get(self, id):
+        print(id)
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-movies-images-by-imdb", "imdb": "tt2935510"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        query_string = configs.get_query_string(
+            request.endpoint, id, None, None)
+        print(query_string)
+        response = requests.request(
+            "GET", url, headers=headers, params=query_string)
         return response.json()
+
 
 class GetSimilarMovies(Resource):
     def get(self, id):
+        print(id)
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-similar-movies", "imdb": "tt2935510","page":"1"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        query_string = configs.get_query_string(
+            request.endpoint, id, None, "1")
+        response = requests.request(
+            "GET", url, headers=headers, params=query_string)
         return response.json()
+
 
 class GetShowsByTitle(Resource):
     def get(self, title):
+        print(title)
+        response_result = []
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-shows-by-title", "title": "thrones"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        return response.json()
+        query_string = configs.get_query_string(
+            request.endpoint, None, title, None)
+        query_by_title_response = requests.request(
+            "GET", url, headers=headers, params=query_string)
+        shows = query_by_title_response.json()
+        print(shows)
+        for show in shows['tv_results']:
+            response = requests.request(
+                "GET", request.url_root + "/get-show-images-by-imdb/"+show['imdb_id'])
+            shows_details = response.json()
+            response_result.append(shows_details)
+        return response_result
+
 
 class GetShowDetails(Resource):
     def get(self, id):
+        print(id)
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-show-details", "imdb": "tt2741602"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        query_string = configs.get_query_string(
+            request.endpoint, id, None, None)
+        response = requests.request(
+            "GET", url, headers=headers, params=query_string)
         return response.json()
 
-class GetShowImages(Resource):
+
+class GetShowImagesByImdb(Resource):
     def get(self, id):
+        print(id)
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-show-images-by-imdb", "imdb": "tt2741602"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        query_string = configs.get_query_string(
+            request.endpoint, id, None, None)
+        print(query_string)
+        response = requests.request(
+            "GET", url, headers=headers, params=query_string)
         return response.json()
+
 
 class GetSimilarShows(Resource):
     def get(self, id):
+        print(id)
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
-        querystring = {"type": "get-similar-shows", "imdb": "tt2741602","page":"1"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        query_string = configs.get_query_string(
+            request.endpoint, id, None, "1")
+        response = requests.request(
+            "GET", url, headers=headers, params=query_string)
         return response.json()
+
+
 class GetIMDBConfigs():
     def get_headers(self):
         self.headers = {
@@ -81,6 +139,20 @@ class GetIMDBConfigs():
             'x-rapidapi-host': os.environ.get('IMDB_API_HOST')
         }
         return self.headers
+
     def get_url(self):
         self.url = os.environ.get('IMDB_BASE_URL')
-        return  self.url 
+        return self.url
+
+    def get_query_string(self, endpoint_type, id, title, page_number):
+
+        if endpoint_type and id and page_number:
+            self.query_string = {"type": endpoint_type,
+                                 "imdb": id, "page": page_number}
+            return self.query_string
+        elif endpoint_type and id:
+            self.query_string = {"type": endpoint_type, "imdb": id}
+            return self.query_string
+        elif endpoint_type and title:
+            self.query_string = {"type": endpoint_type, "title": title}
+            return self.query_string
