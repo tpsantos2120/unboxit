91df119497692ff18436ab7ec9e5db1665d2053f
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-17 01:06:03 +0100

    Removed print statements

--- unboxit/resources/imdb.py
+++ unboxit/resources/imdb.py
@@ -3,91 +3,76 @@ from flask_restful import Resource
 import requests, os
 
 class GetMoviesByTitle(Resource):
-    def get(self):
+    def get(self, title):
+        print(title)
         configs = GetIMDBConfigs()
         url = configs.get_url()
-        print(url)
         headers = configs.get_headers()
-        print(headers)
         querystring = {"type": "get-movies-by-title", "title": "matrix"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetMovieDetails(Resource):
-    def get(self):
+    def get(self, id):
         configs = GetIMDBConfigs()
         url = configs.get_url()
-        print(url)
         headers = configs.get_headers()
-        print(headers)
         querystring = {"type": "get-movie-details", "imdb": "tt2935510"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetMovieImages(Resource):
-    def get(self):
+    def get(self, id):
         configs = GetIMDBConfigs()
         url = configs.get_url()
-        print(url)
         headers = configs.get_headers()
-        print(headers)
         querystring = {"type": "get-movies-images-by-imdb", "imdb": "tt2935510"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetSimilarMovies(Resource):
-    def get(self):
+    def get(self, id):
         configs = GetIMDBConfigs()
         url = configs.get_url()
-        print(url)
         headers = configs.get_headers()
-        print(headers)
         querystring = {"type": "get-similar-movies", "imdb": "tt2935510","page":"1"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetShowsByTitle(Resource):
-    def get(self):
+    def get(self, title):
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
         querystring = {"type": "get-shows-by-title", "title": "thrones"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetShowDetails(Resource):
-    def get(self):
+    def get(self, id):
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
         querystring = {"type": "get-show-details", "imdb": "tt2741602"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetShowImages(Resource):
-    def get(self):
+    def get(self, id):
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
         querystring = {"type": "get-show-images-by-imdb", "imdb": "tt2741602"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 
 class GetSimilarShows(Resource):
-    def get(self):
+    def get(self, id):
         configs = GetIMDBConfigs()
         url = configs.get_url()
         headers = configs.get_headers()
         querystring = {"type": "get-similar-shows", "imdb": "tt2741602","page":"1"}
         response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
         return response.json()
 class GetIMDBConfigs():
     def get_headers(self):
