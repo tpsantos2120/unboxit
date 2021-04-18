f197296f48aaf709c59bc90fa4ca55a99552951c
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-18 22:47:49 +0100

    Added route for viewdetails

--- unboxit/resources/routes.py
+++ unboxit/resources/routes.py
@@ -4,7 +4,7 @@ from .user import LoginUserApi
 from .home import Home
 from .login import Login
 from .register import Register
-from .dashboard import Dashboard
+from .dashboard import Dashboard, ViewDetails
 from .imdb import GetMoviesByTitle, GetMoviesImagesByImdb, GetShowsByTitle, GetShowDetails, GetMovieDetails \
 , GetMoviesImagesByImdb, GetShowImagesByImdb, GetSimilarMovies, GetSimilarShows
 
@@ -18,6 +18,7 @@ def initialize_routes(api):
     api.add_resource(Login, '/login')
     api.add_resource(Register, '/register')
     api.add_resource(Dashboard, '/dashboard')
+    api.add_resource(ViewDetails, '/view-details/<id>/<image>')
     api.add_resource(GetMoviesByTitle, '/get-movies-by-title/<title>', endpoint="get-movies-by-title")
     api.add_resource(GetMovieDetails, '/get-movie-details/<id>', endpoint="get-movie-details")
     api.add_resource(GetMoviesImagesByImdb, '/get-movies-images-by-imdb/<id>', endpoint="get-movies-images-by-imdb")
