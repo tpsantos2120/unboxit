2267eeb7badbd13a8d6bf0125a65fe8e2c9e568e
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-20 18:59:43 +0100

    Commented code to fix heroku error

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from unboxit.app import app
 
 if __name__ == '__main__':
     if os.environ.get('ENV_STATE') == "production":
-        app.config["ENV"] = "production"
+        #app.config["ENV"] = "production"
         app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')))
     elif os.environ.get('ENV_STATE') == "development":
