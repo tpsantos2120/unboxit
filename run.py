2fd4c9c7a9f4fc2bbc8f7af1f22188e2e708ad2d
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-20 21:44:31 +0100

    Removed the wsgi code

--- run.py
+++ run.py
@@ -1,15 +1,12 @@
 import os
 from unboxit import app
-from gevent.pywsgi import WSGIServer
 
 
 if __name__ == '__main__':
     if os.environ.get('ENV_STATE') == "production":
         app.config["ENV"] = "production"
-        # app.run(host=os.environ.get('IP'),
-        #     port=int(os.environ.get('PORT')))
-        http_server = WSGIServer(('', 5000), app)
-        http_server.serve_forever()
+        app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')))
     elif os.environ.get('ENV_STATE') == "development":
         app.config["ENV"] = "development"
         app.run(debug=os.environ.get("DEBUG"))
