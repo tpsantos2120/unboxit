21516d09d3ebf7f11be22d00cdc48d69dd8715af
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-20 19:36:08 +0100

    Changed app settings to fix heroku error

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
 from unboxit.app import app
+from gevent.pywsgi import WSGIServer
+
 
 if __name__ == '__main__':
     if os.environ.get('ENV_STATE') == "production":
         app.config["ENV"] = "production"
-        app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')))
+        # app.run(host=os.environ.get('IP'),
+        #     port=int(os.environ.get('PORT')))
+        http_server = WSGIServer(('', 5000), app)
+        http_server.serve_forever()
     elif os.environ.get('ENV_STATE') == "development":
         app.config["ENV"] = "development"
         app.run(debug=os.environ.get("DEBUG"))
