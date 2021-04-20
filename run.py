bc35c3f4aec575fdea82a90c3f53d8e7465ec89f
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-20 19:48:04 +0100

    Changed wsgi server with gateway

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         app.config["ENV"] = "production"
         # app.run(host=os.environ.get('IP'),
         #     port=int(os.environ.get('PORT')))
-        http_server = WSGIServer(('', 5000), app)
+        http_server = WSGIServer(('0.0.0.0', 5000), app)
         http_server.serve_forever()
     elif os.environ.get('ENV_STATE') == "development":
         app.config["ENV"] = "development"
