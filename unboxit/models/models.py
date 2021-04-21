5df147dd7e4be8d022433649f23823b85e16d5b5
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-22 00:49:52 +0100

    Formatted and added a register rule to delete movie reference from user collection too

--- unboxit/models/models.py
+++ unboxit/models/models.py
@@ -1,17 +1,19 @@
 from enum import unique
 from .db import db
 from flask_bcrypt import generate_password_hash, check_password_hash
+
+
 class Movie(db.Document):
-    title = db.StringField( null=True)
+    title = db.StringField(null=True)
     description = db.StringField(null=True)
-    year = db.StringField( null=True)
-    release_date = db.StringField( null=True)
-    imdb_id = db.StringField( null=True)
-    imdb_rating = db.StringField( null=True)
-    vote_count = db.StringField( null=True)
-    popularity = db.StringField( null=True)
-    youtube_trailer_key = db.StringField( null=True)
-    runtime = db.IntField( null=True)
+    year = db.StringField(null=True)
+    release_date = db.StringField(null=True)
+    imdb_id = db.StringField(null=True)
+    imdb_rating = db.StringField(null=True)
+    vote_count = db.StringField(null=True)
+    popularity = db.StringField(null=True)
+    youtube_trailer_key = db.StringField(null=True)
+    runtime = db.IntField(null=True)
     rated = db.StringField(null=True)
     genres = db.ListField(db.StringField(),  null=True)
     stars = db.ListField(db.StringField(),  null=True)
@@ -22,16 +24,16 @@ class Movie(db.Document):
 
 
 class TvSeries(db.Document):
-    title = db.StringField( null=True)
-    description = db.StringField( null=True)
-    year = db.StringField( null=True)
-    release_date = db.StringField( null=True)
-    imdb_id = db.StringField( null=True)
-    imbd_rating = db.StringField( null=True)
-    vote_count = db.StringField( null=True)
-    popularity = db.StringField( null=True)
-    youtube_trailer_key = db.StringField( null=True)
-    runtime = db.IntField( null=True)
+    title = db.StringField(null=True)
+    description = db.StringField(null=True)
+    year = db.StringField(null=True)
+    release_date = db.StringField(null=True)
+    imdb_id = db.StringField(null=True)
+    imbd_rating = db.StringField(null=True)
+    vote_count = db.StringField(null=True)
+    popularity = db.StringField(null=True)
+    youtube_trailer_key = db.StringField(null=True)
+    runtime = db.IntField(null=True)
     stars = db.ListField(db.StringField(),  null=True)
     creators = db.ListField(db.StringField(),  null=True)
     countries = db.ListField(db.StringField(),  null=True)
@@ -44,14 +46,19 @@ class TvSeries(db.Document):
 class User(db.Document):
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
-    email = db.EmailField( unique=True)
+    email = db.EmailField(unique=True)
     username = db.StringField(required=True)
     password = db.StringField(required=True)
-    movies = db.ListField(db.ReferenceField('Movie', reverse_delete_rule=db.PULL))
-    tv_series = db.ListField(db.ReferenceField('TvSeries', reverse_delete_rule=db.PULL))
+    movies = db.ListField(db.ReferenceField(
+        'Movie', reverse_delete_rule=db.PULL))
+    tv_series = db.ListField(db.ReferenceField(
+        'TvSeries', reverse_delete_rule=db.PULL))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
-    
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+User.register_delete_rule(Movie, 'added_by', db.CASCADE)
