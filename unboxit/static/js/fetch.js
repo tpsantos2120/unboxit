cdbe701771d70fce4bffbd6b79e012231ba394db
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-22 16:17:41 +0100

    Implemented the request function where fecth will be executed

--- unboxit/static/js/fetch.js
+++ unboxit/static/js/fetch.js
@@ -1,15 +1,24 @@
+const _apiHost = window.location.hostname;
+
+async function request(url, params, method = 'GET') {
+
+    
+  
+}
+
+
 function get(url, params) {
-    return request(url, params);
-  }
-  
-  function create(url, params) {
-    return request(url, params, 'POST');
-  }
-  
-   function update(url, params) {
-    return request(url, params, 'PUT');
-  }
-  
-  function remove(url, params) {
-    return request(url, params, 'DELETE');
-  }
+  return request(url, params);
+}
+
+function create(url, params) {
+  return request(url, params, "POST");
+}
+
+function update(url, params) {
+  return request(url, params, "PUT");
+}
+
+function remove(url, params) {
+  return request(url, params, "DELETE");
+}
