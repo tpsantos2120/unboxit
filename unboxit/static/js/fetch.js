9eb41a2857b0f89f7d02d11e35fdbb0fce367f88
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-22 16:11:14 +0100

    Added generic functions to abstract fetch

---
+++ unboxit/static/js/fetch.js
@@ -0,0 +1,15 @@
+function get(url, params) {
+    return request(url, params);
+  }
+  
+  function create(url, params) {
+    return request(url, params, 'POST');
+  }
+  
+   function update(url, params) {
+    return request(url, params, 'PUT');
+  }
+  
+  function remove(url, params) {
+    return request(url, params, 'DELETE');
+  }
