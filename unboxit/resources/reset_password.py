8980c2c72e6d2ab5e1d4285d730ec4831f933254
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-22 01:52:49 +0100

    Setup token creation for user reset password

--- unboxit/resources/reset_password.py
+++ unboxit/resources/reset_password.py
@@ -2,7 +2,10 @@ from unboxit.resources.errors import EmailDoesnotExistsError
 from flask_restful import Resource
 from flask import request, render_template
 from unboxit.models import User
+from unboxit.services.mail_service import send_email
+from flask_jwt_extended import create_access_token
 
+import datetime
 
 class ForgotPassword(Resource):
     def post(self):
@@ -11,8 +14,18 @@ class ForgotPassword(Resource):
         body = request.get_json()
         email = body.get('email')
         if not email:
-            return {"email":"not exist"}
+            raise {"email":"not exist"}
 
         user = User.objects.get(email=email)
         if not user:
             raise EmailDoesnotExistsError
+        expires = datetime.timedelta(hours=24)
+        reset_token = create_access_token(str(user.id), expires_delta=expires)
+
+        return send_email('[Movie-bag] Reset Your Password',
+                              sender='support@movie-bag.com',
+                              recipients=[user.email],
+                              text_body=render_template('email/reset_password.txt',
+                                                        url=url + reset_token),
+                              html_body=render_template('email/reset_password.html',
+                                                        url=url + reset_token))
