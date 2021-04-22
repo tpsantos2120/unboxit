457a6dd32592bb3c04358f70c5c8d4769d1bd975
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-22 16:25:45 +0100

    Added error handling if server responses are not with code 200.

--- unboxit/static/js/fetch.js
+++ unboxit/static/js/fetch.js
@@ -1,36 +1,44 @@
 const _apiHost = window.location.hostname;
 
-async function request(url, params, method = 'GET') {
+async function request(url, params, method = "GET") {
+  const options = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const options = {
-        method,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    };
+  const response = await fetch(_apiHost + url, options);
+  if (response.status !== 200) {
+    return generateErrorResponse(
+      "The server responded with an unexpected status."
+    );
+  }
 
-    const response = await fetch(_apiHost + url, options);
+  const result = await response.json();
 
-    
-    const result = await response.json();
-
-    return result;
-  
+  return result;
 }
 
+function generateErrorResponse(message) {
+    return {
+      status : 'error',
+      message
+    };
+  }
 
 const get = (url, params) => {
   return request(url, params);
-}
+};
 
 const create = (url, params) => {
   return request(url, params, "POST");
-}
+};
 
 const update = (url, params) => {
   return request(url, params, "PUT");
-}
+};
 
 const remove = (url, params) => {
   return request(url, params, "DELETE");
-}
+};
