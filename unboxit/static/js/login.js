0629c43e4341b2bec987adda4a4e9c7ac4f1f286
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-23 21:58:21 +0100

    Removed commented out code

--- unboxit/static/js/login.js
+++ unboxit/static/js/login.js
@@ -1,20 +1,4 @@
 import Fetch from "./Fetch.js";
-// const ready = (id) => {
-//   let isIdExist = false;
-//   // If the body element and the #main element exist
-//   if (document.body && document.querySelector(id)) {
-//     // Run your code here...
-//     // Return so that we don't call requestAnimationFrame() again
-//     isIdExist = true;
-//     return isIdExist;
-//   }
-
-//   // If the body element isn't found, run ready() again at the next pain
-//   window.requestAnimationFrame(ready);
-// };
-
-// // Initialize our ready() function
-// window.requestAnimationFrame(ready);
 
 $(document).ready(function () {
   $("#login-form").validate({
