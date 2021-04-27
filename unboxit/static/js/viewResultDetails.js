13e45fbf514ec302b02a4f980a7109a28e3a2d77
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-27 12:53:11 +0100

    Added a postData function that takes all movie info then post it

--- unboxit/static/js/viewResultDetails.js
+++ unboxit/static/js/viewResultDetails.js
@@ -15,14 +15,14 @@ import Fetch from "./fetch.js";
 const handleSearchDetails = (e) => {
   const id = e.target.getAttribute("imdb");
   const type = e.target.getAttribute("type");
+  const image = e.target.getAttribute("data-src");
+
   console.log(id);
   if (type === "movies") {
-    findMovieDetails(id);
+    findMovieDetails(id, image, type);
   } else {
-    findShowDetails(id);
+    findShowDetails(id, image, type);
   }
-
-  console.log(id, type);
 };
 
 const searchDetails = () => {
@@ -32,18 +32,70 @@ const searchDetails = () => {
   });
 };
 
-const findMovieDetails = async (id) => {
+const addlistener = (id, image, type, e) => {
+  postData(id, image, type);
+};
+
+const postData = async (id, image, type) => {
+  const title = document.querySelector("#title");
+  const description = document.querySelector("#description");
+  const year = document.querySelector("#year");
+  const imdbRating = document.querySelector("#imdb-rating");
+  const voteCount = document.querySelector("#vote-count");
+  const releaseDate = document.querySelector("#release-date");
+  const populatity = document.querySelector("#popularity");
+  const runtime = document.querySelector("#runtime");
+  const stars = document.querySelectorAll(".stars");
+  const directors = document.querySelectorAll(".directors");
+  const creators = document.querySelectorAll(".creators");
+  const data = {
+    imdb_id: id,
+    poster: image,
+    media_type: type,
+    title: title.innerText,
+    description: description.innerText,
+    year: year.innerText,
+    imdb_rating: imdbRating.innerText,
+    vote_count: voteCount.innerText,
+    release_date: releaseDate.innerText,
+    popularity: populatity.innerText,
+    runtime: parseInt(runtime.innerText, 10),
+    stars: [],
+    directors: [],
+    creators: [],
+  };
+  stars.forEach((item) => {
+    data.stars.push(item.innerText);
+  });
+  directors.forEach((item) => {
+    data.directors.push(item.innerText);
+  });
+  creators.forEach((item) => {
+    data.creators.push(item.innerText);
+  });
+  console.log(data);
+  const saveDataResponse = await Fetch.create("/api/movies", data);
+  console.log(saveDataResponse)
+
+};
+
+const findMovieDetails = async (id, image, type) => {
   const viewMovieDetails = document.querySelector("#view-result-details");
   const movieDetails = await Fetch.get("/search/movie/details/", id);
   console.log(movieDetails, viewMovieDetails);
   viewMovieDetails.classList.add("uk-margin-large");
   viewMovieDetails.classList.add("uk-margin-bottom-large");
-
   removeChildrenEl(viewMovieDetails);
   viewMovieDetails.innerHTML = movieDetails;
+  const addButton = document.querySelector("#add-button");
+  if (addButton) {
+    addButton.onclick = addlistener.bind(this, id, image, type);
+  }
 };
+  
+  
 
-const findShowDetails = async (id) => {
+const findShowDetails = async (id, image, type) => {
   const viewShowDetails = document.querySelector("#view-result-details");
   const showDetails = await Fetch.get("/search/show/details/", id);
   console.log(showDetails, viewShowDetails);
@@ -51,6 +103,10 @@ const findShowDetails = async (id) => {
   viewShowDetails.classList.add("uk-margin-bottom-large");
   removeChildrenEl(viewShowDetails);
   viewShowDetails.innerHTML = showDetails;
+  const addButton = document.querySelector("#add-button");
+  if (addButton) {
+    addButton.onclick = addlistener.bind(this, id, image, type);
+  }
 };
 
 const removeChildrenEl = (node) => {
@@ -60,6 +116,5 @@ const removeChildrenEl = (node) => {
 };
 
 export default {
-  
-  searchDetails
+  searchDetails,
 };
