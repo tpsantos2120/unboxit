df6d84519b091e4968b0e8e106e7ff1144059720
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-28 17:35:26 +0100

    Split the fecth requests from the get resource body.

--- unboxit/resources/app_api.py
+++ unboxit/resources/app_api.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import verify_jwt_in_request
 from flask_jwt_extended import jwt_required
 from .jwt import jwt
 import requests
-import json
+import json, random
 
 
 class Dashboard(Resource):
@@ -14,23 +14,38 @@ class Dashboard(Resource):
     def get(self):
         cookie_exist = verify_jwt_in_request(locations=['headers', 'cookies'])
         if cookie_exist:
+            watchlist = []
+            selected_ids = []
             first_name = cookie_exist[1]['sub']['first_name']
             last_name = cookie_exist[1]['sub']['last_name']
-            token = request.cookies.get('access_token_cookie')
-            headers = {"Authorization": "Bearer " + token}
-            data_response = requests.get(
-                request.url_root + 'api/movies', headers=headers)
-            trending_movies_response = requests.get(
-                request.url_root + '/search/trending/movies')
-            treding_movies = trending_movies_response.json()
-            json_data = data_response.json()
-            watchlist = []
+            treding_movies = Dashboard.fetch_treding_movies().json()
+            json_data = Dashboard.fetch_watchlist().json()
             for data in json_data:
+                selected_ids.append(json.loads(data).get('imdb_id'))
                 watchlist.append(json.loads(data))
+            if selected_ids:
+                print(random.choice(selected_ids))
             return make_response(render_template('views/dashboard.html', trending_movies=treding_movies, watchlist=watchlist, title="Dashboard",
-                                                 logged_in=True, first_name=first_name, last_name=last_name), 200, headers)
+                                                 logged_in=True, first_name=first_name, last_name=last_name))
         else:
             return redirect(url_for('home'))
+
+    def fetch_watchlist():
+        token = request.cookies.get('access_token_cookie')
+        headers = {"Authorization": "Bearer " + token}
+        watchlist_response = requests.get(
+            request.url_root + 'api/movies', headers=headers)
+        return watchlist_response
+
+    def fetch_treding_movies():
+        trending_movies_response = requests.get(
+            request.url_root + '/search/trending/movies')
+        return trending_movies_response
+
+    def fetch_recommendations(id):
+        trending_movies_response = requests.get(
+            request.url_root + '/search/trending/movies')
+        return trending_movies_response
 
 
 class DashboardSearch(Resource):
