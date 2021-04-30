3d034d9b927bf44b5d6e4c6197401404e0019bca
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-30 05:41:46 +0100

    removed tvseries model

--- unboxit/models/models.py
+++ unboxit/models/models.py
@@ -1,9 +1,8 @@
-from enum import unique
 from .db import db
 from flask_bcrypt import generate_password_hash, check_password_hash
 
 
-class Movie(db.Document):
+class Watchlist(db.Document):
     poster = db.StringField()
     media_type = db.StringField()
     title = db.StringField()
@@ -22,35 +21,14 @@ class Movie(db.Document):
     creators = db.ListField(db.StringField() )
     added_by = db.ReferenceField('User', unique_with="imdb_id")
 
-
-class TvSeries(db.Document):
-    poster = db.StringField()
-    media_type = db.StringField()
-    title = db.StringField()
-    description = db.StringField()
-    year = db.StringField()
-    release_date = db.StringField()
-    imdb_id = db.StringField(unique=True)
-    imbd_rating = db.StringField()
-    vote_count = db.StringField()
-    popularity = db.StringField()
-    youtube_trailer_key = db.StringField()
-    runtime = db.IntField()
-    stars = db.ListField(db.StringField() )
-    creators = db.ListField(db.StringField() )
-    added_by = db.ReferenceField('User', unique_with="imdb_id")
-
-
 class User(db.Document):
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
     email = db.EmailField(unique=True)
     username = db.StringField(required=True)
     password = db.StringField(required=True)
-    movies = db.ListField(db.ReferenceField(
-        'Movie', reverse_delete_rule=db.PULL))
-    tv_series = db.ListField(db.ReferenceField(
-        'TvSeries', reverse_delete_rule=db.PULL))
+    watchlists = db.ListField(db.ReferenceField(
+        'Watchlist', reverse_delete_rule=db.PULL))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
@@ -59,4 +37,4 @@ class User(db.Document):
         return check_password_hash(self.password, password)
 
 
-User.register_delete_rule(Movie, 'added_by', db.CASCADE)
+User.register_delete_rule(Watchlist, 'added_by', db.CASCADE)
