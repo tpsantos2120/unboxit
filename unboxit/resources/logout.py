cabe723efcbee7bfee31f8fd045a2568b19220c2
tpsantos2120 <thiagocroza472@gmail.com>
2021-04-30 15:56:11 +0100

    Added code to delete cache after logout

--- unboxit/resources/logout.py
+++ unboxit/resources/logout.py
@@ -2,6 +2,7 @@ from flask import Response, request, render_template, make_response, url_for, se
 from flask_jwt_extended.utils import unset_access_cookies
 from flask_jwt_extended.view_decorators import verify_jwt_in_request
 from flask_restful import Resource
+from .cache import cache
 import requests
 
 class Logout(Resource):
@@ -10,6 +11,10 @@ class Logout(Resource):
         if cookie_exist:
             res = make_response(redirect(url_for('home')))
             unset_access_cookies(res)
+            cache.delete('watchlist_cache')
+            cache.delete('recommendation_cache')
+            cache.delete('trending_movies_cache')
+            cache.delete('recommend')
             return res
         else:
             return redirect(url_for('home'))
