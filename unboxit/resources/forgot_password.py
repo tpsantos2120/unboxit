f76236201d01987152835eec86fa3f82a4ca9058
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-02 00:35:03 +0100

    Added logic if there is no token to redirect to home page

--- unboxit/resources/forgot_password.py
+++ unboxit/resources/forgot_password.py
@@ -1,15 +1,11 @@
-from flask import request, render_template, make_response
+from flask import render_template, make_response, redirect
 from flask_restful import Resource
 
 
 class ForgotPasswordReset(Resource):
     def get(self, token):
-        print(token)
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('views/forgot_password.html', title="Password Reset"),200,headers)
-
-# class ForgotPasswordReset(Resource):
-#     def get(self, token):
-#         print(token)
-#         headers = {'Content-Type': 'text/html'}
-#         return make_response(render_template('views/forgot_password_view.html', title="Password Reset"),200,headers)
+        if token:
+            headers = {'Content-Type': 'text/html'}
+            return make_response(render_template('views/forgot_password.html', title="Password Reset"),200,headers)
+        else:
+            return redirect('home')
