a9227c5813bf307d49d39f978c76fb403a7051bc
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-02 04:36:21 +0100

    Added another reset forgotten password resource to get token and modify password.

--- unboxit/resources/user.py
+++ unboxit/resources/user.py
@@ -1,15 +1,16 @@
 import datetime
 from unboxit.services.mail_service import send_email
+from .jwt import jwt
 from flask import Response, request, render_template
 from flask.helpers import make_response
 from flask_jwt_extended import create_access_token
-from flask_jwt_extended.utils import get_jwt_identity, set_access_cookies
+from flask_jwt_extended.utils import decode_token, get_jwt_identity, set_access_cookies
 from flask_jwt_extended.view_decorators import jwt_required
 from unboxit.models.models import User
 from flask_restful import Resource
 from jwt.exceptions import ExpiredSignatureError, DecodeError, InvalidTokenError
 from mongoengine.errors import DoesNotExist, FieldDoesNotExist, NotUniqueError
-from unboxit.resources.errors import EmailDoesNotExistsError, InternalServerError, \
+from unboxit.resources.errors import EmailDoesNotExistsError, ExpiredTokenError, InternalServerError, \
     UnauthorizedError, SchemaValidationError, EmailAlreadyExistsError, BadTokenError
 
 
@@ -20,7 +21,7 @@ class RegisterUserApi(Resource):
             user = User(**body)
             user.hash_password()
             user.save()
-            expires = datetime.timedelta(days=30)
+            expires = datetime.timedelta(days=7)
             user_details = {
                 "user_id": str(user.id),
                 "first_name": user.first_name,
@@ -51,7 +52,7 @@ class LoginUserApi(Resource):
             authorized = user.check_password(body.get('password'))
             if not authorized:
                 raise UnauthorizedError
-            expires = datetime.timedelta(days=30)
+            expires = datetime.timedelta(days=7)
             user_details = {
                 "user_id": str(user.id),
                 "first_name": user.first_name,
@@ -90,7 +91,41 @@ class ResetPassword(Resource):
         except SchemaValidationError:
             raise SchemaValidationError
         except ExpiredSignatureError:
-            raise ExpiredSignatureError
+            raise ExpiredTokenError
+        except (DecodeError, InvalidTokenError):
+            raise BadTokenError
+        except Exception as e:
+            raise InternalServerError
+
+
+class ResetFogottenPassword(Resource):
+    def post(self):
+        try:
+            body = request.get_json()
+            bearer = request.headers.get('Authorization')
+            token = bearer.split()[1]
+            password = body.get('password')
+
+            if not token or not password:
+                raise SchemaValidationError
+
+            user_id = decode_token(token)['sub']['user_id']
+
+            user = User.objects.get(id=user_id)
+
+            user.modify(password=password)
+            user.hash_password()
+            user.save()
+
+            return send_email('[Unboxit] Password reset successful',
+                              sender='contact@tsantos.dev',
+                              recipients=[user.email],
+                              text_body='Password reset was successful',
+                              html_body='<p>Password reset was successful</p>')
+        except SchemaValidationError:
+            raise SchemaValidationError
+        except ExpiredSignatureError:
+            raise ExpiredTokenError
         except (DecodeError, InvalidTokenError):
             raise BadTokenError
         except Exception as e:
@@ -110,7 +145,7 @@ class ForgotPassword(Resource):
             if not user:
                 raise EmailDoesNotExistsError
 
-            expires = datetime.timedelta(hours=24)
+            expires = datetime.timedelta(minutes=60)
             payload = {"user_id": str(user.id)}
             reset_token = create_access_token(payload, expires_delta=expires)
 
