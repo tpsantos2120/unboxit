a7182126825d7b716180171b6e66c611d72e167d
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 06:48:59 +0100

    Added test to login without payload

--- unit_tests/test_user_login.py
+++ unit_tests/test_user_login.py
@@ -101,3 +101,32 @@ class TestUserLogin(BaseCase):
         self.assertEqual("Invalid username or password",
                          response.json['message'])
         self.assertEqual(401, response.status_code)
+
+    def test_login_with_without_payload(self):
+        first_name = "Darth"
+        last_name = "Vader"
+        username = "darkside"
+        email = "darth@darkside.com"
+        password = "force1234"
+
+        payload_register = json.dumps({
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": username,
+            "email": email,
+            "password": password
+        })
+
+        payload_login = json.dumps({})
+
+        response = self.app.post('/api/auth/signup',
+                                 headers={"Content-Type": "application/json"},
+                                 data=payload_register)
+
+        response = self.app.post('/api/auth/login',
+                                 headers={"Content-Type": "application/json"},
+                                 data=payload_login)
+
+        self.assertEqual("Invalid username or password",
+                         response.json['message'])
+        self.assertEqual(401, response.status_code)
