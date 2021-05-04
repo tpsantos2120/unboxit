fb50057cddea7fd6d5a469b0a0f78b879b7c16ef
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 03:45:16 +0100

    Updated the run file as i moved the config logic to a config file

--- run.py
+++ run.py
@@ -1,13 +1,5 @@
-import os
 from unboxit import app
 
 
 if __name__ == '__main__':
-    if os.environ.get('ENV_STATE') == "production":
-        app.config["ENV"] = "production"
-        app.config["FLASK_ENV"] = "production"
-        app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT'), threaded=True))
-    elif os.environ.get('ENV_STATE') == "development":
-        app.config["ENV"] = "development"
-        app.run(debug=os.environ.get("DEBUG"))
+    app.run()
