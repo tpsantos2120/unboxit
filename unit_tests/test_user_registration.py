a4dbb510d7744c133a630b12e0dd4ee7a5ea07ca
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 06:01:39 +0100

    Added test for already existing user

--- unit_tests/test_user_registration.py
+++ unit_tests/test_user_registration.py
@@ -72,3 +72,27 @@ class TestUserRegistration(BaseCase):
         self.assertEqual('Something went wrong internally',
                          response.json['message'])
         self.assertEqual(500, response.status_code)
+
+    def test_registering_already_existing_user(self):
+
+        first_name = "Darth"
+        last_name = "Vader"
+        username = "darkside"
+        email = "darth@darkside.com"
+        password = "force1234"
+        payload_register = json.dumps({
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": username,
+            "email": email,
+            "password": password
+        })
+        response = self.app.post(
+            '/api/auth/register', headers={"Content-Type": "application/json"}, data=payload_register)
+
+        response = self.app.post(
+            '/api/auth/register', headers={"Content-Type": "application/json"}, data=payload_register)
+
+        self.assertEqual(
+            'User with given email address already exists', response.json['message'])
+        self.assertEqual(400, response.status_code)
