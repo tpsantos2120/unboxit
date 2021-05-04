fd810fbccc49b9ac2efa688624008cc37006941f
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 06:42:03 +0100

    Added test for login with invalid email

--- unit_tests/test_user_login.py
+++ unit_tests/test_user_login.py
@@ -11,6 +11,7 @@ class TestUserLogin(BaseCase):
         username = "darkside"
         email = "darth@darkside.com"
         password = "force1234"
+
         payload_register = json.dumps({
             "first_name": first_name,
             "last_name": last_name,
@@ -34,3 +35,36 @@ class TestUserLogin(BaseCase):
 
         self.assertEqual(str, type(response.json['token']))
         self.assertEqual(200, response.status_code)
+
+    def test_login_with_invalid_email(self):
+        first_name = "Darth"
+        last_name = "Vader"
+        username = "darkside"
+        email = "darth@darkside.com"
+        password = "force1234"
+
+        payload_register = json.dumps({
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": username,
+            "email": email,
+            "password": password
+        })
+
+        email = "luke@skywalker.com"
+
+        payload_login = json.dumps({
+            "email": email,
+            "password": password
+        })
+        response = self.app.post('/api/auth/signup',
+                                 headers={"Content-Type": "application/json"},
+                                 data=payload_register)
+
+        response = self.app.post('/api/auth/login',
+                                 headers={"Content-Type": "application/json"},
+                                 data=payload_login)
+
+        self.assertEqual("Invalid username or password",
+                         response.json['message'])
+        self.assertEqual(401, response.status_code)
