f2aef3de69fb88eb05b003b19a0c3cbd0e91c5b1
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 04:36:57 +0100

    Updated run file to run based on environment

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+import os
 from unboxit import app
 
 
 if __name__ == '__main__':
-    app.run()
+    if os.environ.get('ENV') == "production":
+        app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')))
+    else:
+        app.run()
