8f299f7e017a5aa087ab24ccfb93ce473e79e79f
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 05:41:15 +0100

    Added test to register with non existing field

--- unit_tests/test_user_registration.py
+++ unit_tests/test_user_registration.py
@@ -24,3 +24,25 @@ class TestUserRegistration(BaseCase):
 
         self.assertEqual(str, type(response.json['token']))
         self.assertEqual(200, response.status_code)
+
+    def test_register_with_non_existing_field(self):
+        first_name = "Darth"
+        last_name = "Vader"
+        username = "darkside"
+        email = "darth@darkside.com"
+        password = "force1234"
+        hobby = "kill people"
+        payload_register = json.dumps({
+            "last_name": last_name,
+            "username": username,
+            "email": email,
+            "password": password,
+            "hobby": hobby
+        })
+
+        response = self.app.post(
+            '/api/auth/register', headers={"Content-Type": "application/json"}, data=payload_register)
+
+        self.assertEqual('Request is missing required fields',
+                         response.json['message'])
+        self.assertEqual(400, response.status_code)
