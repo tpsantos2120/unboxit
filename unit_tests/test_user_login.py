101527d09c220482468928e768ac4a756cdc0794
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 06:45:27 +0100

    Added login test for user with invalid password

--- unit_tests/test_user_login.py
+++ unit_tests/test_user_login.py
@@ -68,3 +68,36 @@ class TestUserLogin(BaseCase):
         self.assertEqual("Invalid username or password",
                          response.json['message'])
         self.assertEqual(401, response.status_code)
+
+    def test_login_with_invalid_password(self):
+        first_name = "Darth"
+        last_name = "Vader"
+        username = "darkside"
+        email = "darth@darkside.com"
+        password = "force1234"
+
+        payload_register = json.dumps({
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": username,
+            "email": email,
+            "password": password
+        })
+
+        password = "dark1234"
+
+        payload_login = json.dumps({
+            "email": email,
+            "password": password
+        })
+        response = self.app.post('/api/auth/signup',
+                                 headers={"Content-Type": "application/json"},
+                                 data=payload_register)
+
+        response = self.app.post('/api/auth/login',
+                                 headers={"Content-Type": "application/json"},
+                                 data=payload_login)
+
+        self.assertEqual("Invalid username or password",
+                         response.json['message'])
+        self.assertEqual(401, response.status_code)
