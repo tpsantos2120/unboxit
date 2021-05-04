c9001e6023b25a2e07aa35ac0b70d788c6d7ffec
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-04 03:57:39 +0100

    MOved 500s and 404s away from app file.

---
+++ unboxit/resources/app_api/error_handler.py
@@ -0,0 +1,9 @@
+from flask import render_template
+
+
+class ErrorHandler():
+    def page_not_found(e):
+        return render_template('components/error_404.html'), 404
+
+    def internal_error(e):
+        return render_template('components/error_500.html'), 500
