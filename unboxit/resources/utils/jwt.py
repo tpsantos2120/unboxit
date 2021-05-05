4a93138f55c22bec5e870e4ad8b690af9c07a1d6
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-05 08:25:36 +0100

    Added generic jwt unauthorized handler so I dont have to include that code with every route

--- unboxit/resources/utils/jwt.py
+++ unboxit/resources/utils/jwt.py
@@ -1,6 +1,12 @@
+from flask.helpers import make_response, url_for
 from flask_jwt_extended import JWTManager
+from werkzeug.utils import redirect
 
 jwt = JWTManager()
 
 def initialize_jwt(app):
     jwt.init_app(app)
+    
+    @jwt.unauthorized_loader
+    def not_authorized(callback):
+        return make_response(redirect(url_for('home')))
