5ea88e40e1160633ff3d3b5d84e1567f921e994d
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-07 02:57:52 +0100

    Added handlers in case jwt is invalid or fails to verify

--- unboxit/resources/utils/jwt.py
+++ unboxit/resources/utils/jwt.py
@@ -1,4 +1,4 @@
-from flask.helpers import make_response, url_for
+from flask import make_response, url_for, render_template
 from flask_jwt_extended import JWTManager
 from werkzeug.utils import redirect
 
@@ -10,3 +10,43 @@ def initialize_jwt(app):
         Register the JWT instance with the Flask app.
     """
     jwt.init_app(app)
+
+
+@jwt.unauthorized_loader
+def not_authorized(callback):
+    """
+        Handle 401 errors with redirect
+        when token does not exist.
+    """
+    return make_response(render_template(
+        "components/401.html"))
+
+
+@jwt.expired_token_loader
+def expired_token(jwt_header, payload):
+    """
+        Handle 401 errors with redirect
+        when token does not exist.
+    """
+    return make_response(render_template(
+        "components/401.html"))
+
+
+@jwt.token_verification_failed_loader
+def token_verification_failed(jwt_header, payload):
+    """
+        Handle 401 errors with redirect
+        when token does not exist.
+    """
+    return make_response(render_template(
+        "components/401.html"))
+
+
+@jwt.invalid_token_loader
+def invalid_token(callback):
+    """
+        Handle 401 errors with redirect
+        when token does not exist.
+    """
+    return make_response(render_template(
+        "components/401.html"))
