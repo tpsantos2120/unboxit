514bf331a59fcf023803c5f3517db3a319371b73
tpsantos2120 <thiagocroza472@gmail.com>
2021-05-07 01:25:44 +0100

    Updated to comply with PEP8 styles

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from unboxit import app
 if __name__ == '__main__':
     if os.environ.get('ENV') == "production":
         app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')))
+                port=int(os.environ.get('PORT')))
     else:
         app.run()
